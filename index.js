93062689f4d04fa7d3b31e3730bf0d39ef92318d
飞龙project <flzs6137@gmail.com>
2021-12-04 18:44:02 +0800

    大概写完了

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ const subItem = [
 
 
 async function handleRequest(request) {
-
+  var sum=""
   for (i = 0; i < subItem.length; i++) {
     await fetch(subItem[i]).then(res => {
       return res.text()
     }).then(res=>{
-      console.log(res)
+      console.log(atob(res))
+      sum+=atob(res)
     })
   }
 
-  return new Response("ok")
+  return new Response(btoa(sum))
 }
 
 addEventListener("fetch", (event) => {
+  console.clear()
   event.respondWith(
     handleRequest(event.request).catch(
       (err) => new Response(err.stack, { status: 500 })
